8af706f2fc55dd07c3ba67c7af2f3caab423aec1
Orstein <esteban.yima@gmail.com>
2018-01-18 15:30:07 -0300

    3

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name':'Chilean Accounting Reports',
+    'description':'Formato de Balance de 8 Columnas para CHile',
+    'category': 'Localization',
+    'version':'0.1',
+    'author':'Konos',
+    'website':'http://konos.cl',
+    'data': [
+
+         'views/layout.xml',
+
+    ],
+    'depends': [
+                'account',
+              
+                ],
+}
